df83053d1d96d3fea57a1ce2e1ca102a3021a155
arNparad0x <60833714+arNparad0x@users.noreply.github.com>
2020-02-13 13:20:24 +0530

    Add files via upload

---
+++ ytdl.sh
@@ -0,0 +1,11 @@
+pkg install python -y
+pip install youtube-dl 
+mkdir /data/data/com.termux/files/home/storage/shared/Youtube 
+mkdir -p ~/.config/youtube-dl
+cp yt/config ~/.config/youtube-dl/config 
+mkdir ~/bin
+cp yt/termux-url-opener ~/bin/termux-url-opener
+printf "\\033[0;31m\n"
+cat me
+printf "\\033[0;37m\n"
+
